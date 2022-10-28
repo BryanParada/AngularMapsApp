7fbab74c6dcd57b10344225ba4b6c6e22fc9c5e4
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-28 10:51:32 -0300

    V310 - App.component - center, zoom map

--- src/app/maps/pages/full-screen/full-screen.component.ts
+++ src/app/maps/pages/full-screen/full-screen.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js'); <--importacion require de Mapbox
 // se debe instalar "npm i --save-dev @types/mapbox-gl" y dejar como abajo:
 import * as mapboxgl from "mapbox-gl";
-import { environment } from '../../../../environments/environment.prod';
+
 
 @Component({
   selector: 'app-full-screen',
@@ -21,12 +21,12 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    
-  (mapboxgl as any).accessToken = environment.MapBoxToken;
+ 
   var map = new mapboxgl.Map({
     container: 'mapContainer',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [ -70.583583,-33.568053],
+    zoom: 15
   });
 
   }
