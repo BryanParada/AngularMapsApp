8b43b3df6e39d0e932321009615ef0447e011044
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-28 10:28:51 -0300

    V308 - MenuItem Routerlink

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  route: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
+  styles: [`
+  li{
+    cursor: pointer;
+  }
+  `
   ]
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class MenuComponent {
+ 
+menuItem: MenuItem[] = [
+  {
+    route: '/maps/fullscreen',
+    name: 'FullScreen'
+  },
+  {
+    route: '/maps/zoom-range',
+    name: 'ZoomRange'
+  },
+  {
+    route: '/maps/bookmarks',
+    name: 'Bookmarks'
+  },
+  {
+    route: '/maps/properties',
+    name: 'Properties'
+  },
+]
 
 }
