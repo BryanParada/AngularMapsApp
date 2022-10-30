c39913f833899d2faa11e0eca19031e5b28c26f9
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-30 13:50:12 -0300

    V317 - bookmarks

--- src/app/maps/pages/bookmarks/bookmarks.component.ts
+++ src/app/maps/pages/bookmarks/bookmarks.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef,  ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styles: [
+    `
+     .mapContainer{
+      width: 100%;
+      height: 100%;
+    }
+    `
   ]
 })
-export class BookmarksComponent implements OnInit {
+export class BookmarksComponent implements AfterViewInit {
 
+  @ViewChild('mapBookmark') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+
+  zoomLevel: number = 15;
+  center: [number, number] = [ -70.583583,-33.568053]
+  
   constructor() { }
+  ngAfterViewInit(): void {
+   
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = 'Hola';
+
+    new mapboxgl.Marker({
+      //element: markerHtml // para personalizar icono
+    })
+       .setLngLat(this.center)
+       .addTo( this.map );
+                      
+
+  }
 
   ngOnInit(): void {
   }
